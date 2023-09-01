79a19ae3049237e1dd4f36afd444804d9cbc6ca9
Dustin Wleczyk <dustinwleczyk@hotmail.com>
2023-09-01 14:14:03 -0500

    Added spring boot and the application.yml to exclude the datasource for now

---
+++ src/main/java/com/example/DMPlayerWebsite/DmPlayerWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.DMPlayerWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmPlayerWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmPlayerWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DMPlayerWebsite/DmPlayerWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DMPlayerWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmPlayerWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
